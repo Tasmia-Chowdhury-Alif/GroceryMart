800cf2e3eb2111419c799193ef887e8f96fd0683
Tasmia-Chowdhury-Alif <tasmiachowdhuryalif222@gmail.com>
2025-07-15 00:31:34 +0600

    Migrated Profile models user field to OneToOneField and updated image upload path to media root

--- GroceryMart_api/accounts/models.py
+++ GroceryMart_api/accounts/models.py
@@ -3,11 +3,11 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Profile(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='profile')
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
     full_name = models.CharField(max_length=200)
     phone = models.CharField(max_length=20)
     address = models.TextField()
-    image = models.ImageField(upload_to='users/profile/', null=True, blank=True)
+    image = models.ImageField(upload_to='accounts/profile/', null=True, blank=True)
     balance = models.DecimalField(default= 0, max_digits= 12, decimal_places= 2)
     shopping_prefs = models.JSONField(default=dict, blank=True)
 
