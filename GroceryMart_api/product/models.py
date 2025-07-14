de5f6141e8c4e86e80c80e91e4448fc9706472b6
Tasmia-Chowdhury-Alif <tasmiachowdhuryalif222@gmail.com>
2025-07-15 00:33:43 +0600

    added image field to Product model

--- GroceryMart_api/product/models.py
+++ GroceryMart_api/product/models.py
@@ -24,7 +24,8 @@ class Brand(models.Model):
 class Product(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True)
-    is_digital = models.BooleanField(default=False)
+    image = models.ImageField(upload_to='products/profile/', null=True, blank=True)
+    is_digital = models.BooleanField(default=False) # digital means does it requires delevery 
     brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
     category = TreeForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
     
