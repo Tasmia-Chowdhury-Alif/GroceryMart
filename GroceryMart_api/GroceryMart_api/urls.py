4f961498bc9703df92e2d82ed42c7d2db2c32aaa
Tasmia-Chowdhury-Alif <tasmiachowdhuryalif222@gmail.com>
2025-07-15 20:01:23 +0600

    added product app url

--- GroceryMart_api/GroceryMart_api/urls.py
+++ GroceryMart_api/GroceryMart_api/urls.py
@@ -4,21 +4,14 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-from rest_framework.routers import DefaultRouter
 
-from product.views import CategoryViewSet
-
-
-router = DefaultRouter()
-
-router.register("categories", CategoryViewSet)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("auth/", include("djoser.urls")),  # /auth/users/, /auth/users/me/
     path("auth/", include("djoser.urls.jwt")),  # /auth/jwt/create/, etc.
     path("accounts/", include("accounts.urls")),
-    path("categories/", include(router.urls)),
+    path("product/", include("product.urls")),
 ]
 
 urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
