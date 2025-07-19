5e56797f43c0a766f10cb10bf2545a68092697de
Tasmia-Chowdhury-Alif <tasmiachowdhuryalif222@gmail.com>
2025-07-19 14:03:42 +0600

    created CategoryModelAdmin by inheriting MPTTModelAdmin and prepopulated_fields for slug field

--- GroceryMart_api/product/admin.py
+++ GroceryMart_api/product/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 
 from .models import Category, Brand, Product
+from mptt.admin import MPTTModelAdmin
+
+class CategoryModelAdmin(MPTTModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
 
 # Register your models here.
-admin.site.register(Category)
+admin.site.register(Category, CategoryModelAdmin)
 admin.site.register(Brand)
 admin.site.register(Product)
