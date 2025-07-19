eb8e3b6bd2f03e34934643f3a005a7fed970a898
Tasmia-Chowdhury-Alif <tasmiachowdhuryalif222@gmail.com>
2025-07-19 14:01:05 +0600

    added slug field for Category, added created_at and updated_at field and ordering Meta class

--- GroceryMart_api/product/models.py
+++ GroceryMart_api/product/models.py
@@ -5,6 +5,7 @@ from mptt.models import MPTTModel, TreeForeignKey
 # Create your models here.
 class Category(MPTTModel):
     name = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=150)
     parent = TreeForeignKey("self", on_delete=models.CASCADE, null=True, blank=True)
 
     class MPTTMeta:
@@ -17,6 +18,9 @@ class Category(MPTTModel):
 class Brand(models.Model):
     name = models.CharField(max_length=100)
 
+    class Meta:
+        ordering = ['name']
+
     def __str__(self):
         return self.name
 
@@ -24,12 +28,17 @@ class Brand(models.Model):
 class Product(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField(blank=True)
-    price = models.DecimalField(max_digits= 12, decimal_places= 2)
+    price = models.DecimalField(max_digits= 10, decimal_places= 2)
     stock = models.PositiveIntegerField()
     image = models.ImageField(upload_to='products/', null=True, blank=True)
     brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
     category = TreeForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
     is_digital = models.BooleanField(default=False) # digital means does it requires delevery 
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
     
+    class Meta:
+        ordering = ['-created_at']
+
     def __str__(self):
         return self.name
