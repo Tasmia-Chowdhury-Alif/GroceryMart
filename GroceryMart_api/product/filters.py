a894e2447aa060f2b6540d043c76ec6bbce365ab
Tasmia-Chowdhury-Alif <tasmiachowdhuryalif222@gmail.com>
2025-07-21 01:02:55 +0600

    created Custom FilterSet Class

---
+++ GroceryMart_api/product/filters.py
@@ -0,0 +1,10 @@
+from django_filters import rest_framework as filters
+from .models import Product
+
+class ProductFilter(filters.FilterSet):
+    min_price = filters.NumberFilter(field_name="price", lookup_expr="gte")  # Greater than or equal to
+    max_price = filters.NumberFilter(field_name="price", lookup_expr="lte")  # Less than or equal to
+
+    class Meta:
+        model = Product
+        fields = ["brand", "category", "min_price", "max_price"]
