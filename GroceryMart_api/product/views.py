668c0e6d903eb7e21e1515c5b781e29b7eba0a56
Tasmia-Chowdhury-Alif <tasmiachowdhuryalif222@gmail.com>
2025-07-21 01:04:39 +0600

    updated ProductViewSet for filtering and ordering

--- GroceryMart_api/product/views.py
+++ GroceryMart_api/product/views.py
@@ -1,32 +1,35 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, filters
 from rest_framework.response import Response
 from .models import Category, Brand, Product
 from .serializers import CategorySerializer, BrandSerializer, ProductSerializer
 
+from django_filters.rest_framework import DjangoFilterBackend
+from .filters import ProductFilter
 
-class CategoryViewSet(viewsets.ViewSet):
-    # A simple Viewset for viewing all categories
+
+class CategoryViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Category.objects.all()
-
-    def list(self, request):
-        serializer = CategorySerializer(self.queryset, many=True)
-        return Response(serializer.data)
+    serializer_class = CategorySerializer
 
 
-class BrandViewSet(viewsets.ViewSet):
-    # A simple Viewset for viewing all categories
+class BrandViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Brand.objects.all()
-
-    def list(self, request):
-        serializer = BrandSerializer(self.queryset, many=True)
-        return Response(serializer.data)
+    serializer_class = BrandSerializer
 
 
-class ProductViewSet(viewsets.ViewSet):
+class ProductViewSet(viewsets.ModelViewSet):
     # A simple Viewset for viewing all categories
     queryset = Product.objects.all()
+    serializer_class = ProductSerializer
 
-    def list(self, request):
-        serializer = ProductSerializer(self.queryset, many=True)
-        return Response(serializer.data)
+    filter_backends = [
+        filters.SearchFilter,
+        DjangoFilterBackend,
+        filters.OrderingFilter,
+    ]
+    search_fields = ["name", "description"]  # ?search=milk
+    filterset_class = ProductFilter  # ?brand=1&category=2 / ?min_price=10&max_price=50
+    ordering_fields = ["price", "created_at"]  # ?ordering=price / ?ordering=-created_at ( use - sign for decending ordering )
+
+    
