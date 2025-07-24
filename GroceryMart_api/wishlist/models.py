cf5ba3f871ab55407d3322626ba58f7d3e997b46
Tasmia-Chowdhury-Alif <tasmiachowdhuryalif222@gmail.com>
2025-07-24 18:37:55 +0600

    created Wishlist and WishlistItem models

---
+++ GroceryMart_api/wishlist/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.contrib.auth.models import User
+from product.models import Product
+
+class Wishlist(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="wishlist")
+
+    def __str__(self):
+        return f"{self.user.username}'s Wishlist"
+
+
+class WishlistItem(models.Model):
+    wishlist = models.ForeignKey(Wishlist, on_delete=models.CASCADE, related_name="items")
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ['wishlist', 'product']
+
+    def __str__(self):
+        return f"{self.product.name} in {self.wishlist.user.username}'s Wishlist"
