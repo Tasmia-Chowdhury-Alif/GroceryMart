c278063fd39c56570288a705e29f01de967f06a3
Tasmia-Chowdhury-Alif <tasmiachowdhuryalif222@gmail.com>
2025-07-25 18:54:33 +0600

    updated Prodect models stock field to default=0

--- GroceryMart_api/product/models.py
+++ GroceryMart_api/product/models.py
@@ -29,7 +29,7 @@ class Product(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField(blank=True)
     price = models.DecimalField(max_digits= 10, decimal_places= 2)
-    stock = models.PositiveIntegerField()
+    stock = models.PositiveIntegerField(default=0)
     image = models.ImageField(upload_to='products/', null=True, blank=True)
     brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
     category = TreeForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
