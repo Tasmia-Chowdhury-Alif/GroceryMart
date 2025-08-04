c6a1a6d2cae141cf8c43201af4b07fe2af5b1277
Tasmia-Chowdhury-Alif <tasmiachowdhuryalif222@gmail.com>
2025-08-04 22:08:00 +0600

    removed unncessery comma

--- GroceryMart_api/cart/admin.py
+++ GroceryMart_api/cart/admin.py
@@ -2,5 +2,5 @@ from django.contrib import admin
 from . import models
 
 # Register your models here.
-admin.site.register(models.Cart),
-admin.site.register(models.CartItem),
+admin.site.register(models.Cart)
+admin.site.register(models.CartItem)
