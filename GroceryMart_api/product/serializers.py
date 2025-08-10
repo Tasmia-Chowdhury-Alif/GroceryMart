7fdf6f19ffdca630bb4efa7fedad854ab503dd73
Tasmia-Chowdhury-Alif <tasmiachowdhuryalif222@gmail.com>
2025-08-10 17:40:19 +0600

    feat: added ReviewSerializer and updated ProductSerializer

--- GroceryMart_api/product/serializers.py
+++ GroceryMart_api/product/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 
-from .models import Category, Brand, Product
+from .models import Category, Brand, Product, Review
+from accounts.serializers import UserSerializer
 
 
 class CategorySerializer(serializers.ModelSerializer):
@@ -15,9 +16,34 @@ class BrandSerializer(serializers.ModelSerializer):
         fields = "__all__"
 
 
+class ReviewSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True)
+    product = serializers.PrimaryKeyRelatedField(queryset=Product.objects.all())
+
+    class Meta:
+        model = Review
+        fields = ['id', 'product', 'user', 'rating', 'comment', 'created_at', 'updated_at']
+        read_only_fields = ['user', 'created_at', 'updated_at']
+
+    def validate_rating(self, value):
+        if value < 1 or value > 5:
+            raise serializers.ValidationError("Rating must be between 1 and 5.")
+        return value
+
+
 class ProductSerializer(serializers.ModelSerializer):
     brand = BrandSerializer()
     category = CategorySerializer()
+    reviews = ReviewSerializer(many=True, read_only=True)
+    average_rating = serializers.SerializerMethodField()
+
     class Meta:
         model = Product
-        fields = "__all__"
+        fields = [
+            'id', 'name', 'description', 'price', 'stock', 'image', 'brand',
+            'category', 'is_digital', 'created_at', 'updated_at', 'reviews', 'average_rating'
+        ]
+
+    def get_average_rating(self, obj): # obj is the Product instance
+        return obj.average_rating()
+
