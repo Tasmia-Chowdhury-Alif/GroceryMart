d4906e3d678d61a777729edad53d3fa9e6a0a246
Tasmia-Chowdhury-Alif <tasmiachowdhuryalif222@gmail.com>
2025-10-04 23:49:39 +0600

    feat(orders): add payment_method field to Order model

--- GroceryMart_api/orders/serializers.py
+++ GroceryMart_api/orders/serializers.py
@@ -3,6 +3,7 @@ from .models import Order, OrderItem
 
 
 class OrderItemSerializer(serializers.ModelSerializer):
+    """Serializer for order item."""
     product_name = serializers.ReadOnlyField(source="product.name")
 
     class Meta:
@@ -11,8 +12,9 @@ class OrderItemSerializer(serializers.ModelSerializer):
 
 
 class OrderSerializer(serializers.ModelSerializer):
+    """Serializer for orders, including items."""
     items = OrderItemSerializer(many=True, read_only=True)
 
     class Meta:
         model = Order
-        fields = ["id", "user", "total", "status", "created_at", "items"]
+        fields = ["id", "user", "total", "status", "created_at", "items", "payment_method"]
