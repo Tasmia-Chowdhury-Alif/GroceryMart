856c69cf0336d216cbd4a46652e43a978f8e71d6
Tasmia-Chowdhury-Alif <tasmiachowdhuryalif222@gmail.com>
2025-10-05 20:54:12 +0600

    updatd configuration for vercel deployment

--- GroceryMart_api/GroceryMart_api/wsgi.py
+++ GroceryMart_api/GroceryMart_api/wsgi.py
@@ -8,11 +8,16 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise
+from pathlib import Path
+
+# Set the project base directory
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'GroceryMart_api.settings')
 
 application = get_wsgi_application()
+application = WhiteNoise(application, root=os.path.join(BASE_DIR, 'staticfiles'), prefix='static/')
 
 app = application
