652b1748d95823957df31a7fc14d654fe9edc6b4
Tasmia-Chowdhury-Alif <tasmiachowdhuryalif222@gmail.com>
2025-10-06 22:18:01 +0600

    feat: Implement API root view

--- GroceryMart_api/cart/urls.py
+++ GroceryMart_api/cart/urls.py
@@ -2,12 +2,14 @@ from django.urls import path
 from .views import CartViewSet
 
 
+app_name = 'cart'
+
 cart_list = CartViewSet.as_view({
     'get' : 'list',
 })
 
 urlpatterns = [
-    path('', cart_list),
+    path('', cart_list, name='cart-list'),
     path('add/', CartViewSet.as_view({'post' : 'add_item'})),
     path('remove/', CartViewSet.as_view({'post' : 'remove_item'})),
     path('items/<int:pk>/update-quantity/', CartViewSet.as_view({'patch': 'update_quantity'})),
